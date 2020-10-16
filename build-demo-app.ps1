c0ca0fcbe8b72bc98f7ef2a1b8aba64dfbac3d33
stavroskasidis <stavroskasidis@outlook.com>
2020-10-16 22:09:00 +0300

    added demo app build script

---
+++ build-demo-app.ps1
@@ -0,0 +1,39 @@
+Push-Location $PSScriptRoot
+
+function Write-Message{
+    param([string]$message)
+    Write-Host
+    Write-Host $message
+    Write-Host
+}
+function Confirm-PreviousCommand {
+    param([string]$errorMessage)
+    if ( $LASTEXITCODE -ne 0) { 
+        if( $errorMessage) {
+            Write-Message $errorMessage
+        }    
+        exit $LASTEXITCODE 
+    }
+}
+
+function Confirm-Process {
+    param ([System.Diagnostics.Process]$process,[string]$errorMessage)
+    $process.WaitForExit()
+    if($process.ExitCode -ne 0){
+        Write-Host $process.ExitCode
+        if( $errorMessage) {
+            Write-Message $errorMessage
+        }    
+        exit $process.ExitCode 
+    }
+}
+
+# Check prerequisites
+$proc = Start-Process "dotnet" -ArgumentList "--version" -PassThru
+Confirm-Process $proc "Could not find dotnet sdk, please install and run again ..."
+
+Write-Message "Building ..."
+dotnet publish DemoApp\BlazorDialog.DemoApp\Server -c Release -o artifacts\demoapp
+Confirm-PreviousCommand
+
+Write-Message "Build completed successfully"
